1fc0cef75db04c6d73430762592e6f9943809d2d
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-11 20:02:42 -0500

    Add initial definitions.

---
+++ src/@types/js-xdr/index.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for js-xdr v1.1.1
+// Project: https://github.com/stellar/js-xdr
+// Definitions by: Adolfo Builes <https://github.com/abuiles>
+// Timur Ramazanov <charlie.vmk@gmail.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.4.1
+declare module 'js-xdr' {
+  import * as Long from 'long'
+
+  export class Array {
+    _childType: any
+    _length: number
+  }
+
+  export class ChildStruct extends Struct {
+  }
+
+  export function config(fn: any, types?: any): any;
+
+  export class Enum {
+    name: string
+    value: number
+  }
+
+  export class Hyper extends Long {
+  }
+
+  export class Opaque {
+  }
+
+  export class Struct {
+    _attributes: object
+  }
+
+  export class Union {
+    switch(): any
+    armType(): any
+    value(): any
+    arm(): any
+  }
+
+  export class UnsignedHyper extends Long {
+  }
+
+  export class VarArray {
+    _childType: any
+    _length: number
+  }
+
+  export class VarOpaque {
+  }
+}
